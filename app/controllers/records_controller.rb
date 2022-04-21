91dc37e8b37f8af03fb6360fd59c8e0d71806533
Alexandre Andrade <aaca91@gmail.com>
2022-04-21 20:13:17 -0300

    record controller updated (audio analysis still missing filename logic), changed home password

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -7,17 +7,27 @@ class RecordsController < ApplicationController
     @record = Record.new
   end
 
-  # https://firebasestorage.googleapis.com/v0/b/hear4me-e0481.appspot.com/o/audio%2FY2022_M3_D19_H14_M9_rand80940.ogg?alt=media&token=cc8bda74-fc45-4a9a-b1c4-800714a4f8ba
-  # https://firebasestorage.googleapis.com/v0/b/hear4me-e0481.appspot.com/o/audio%2FY2022_M3_D19_H22_M8_rand80940.ogg?alt=media&token=d859ffdf-4742-4e83-b82d-ba1c3a3fc957
-  # Y2022_M3_D19_H22_M8_rand80940.ogg
+  # # TODO
+  # def analyse_audio_request
+  #   audio_file = params['audio']
+  #   request_params = { audio_file: audio_file, filename: 'TODO' }
+  #   method = 'analyze_audio'
+  #   api_request(request_params, method)
+  # end
 
-  def transcription
-    audio = params['audio']
-    uri = URI('https://api.nasa.gov/planetary/apod')
-    params = { :api_key => 'your_api_key' }
-    uri.query = URI.encode_www_form(params)
+  def analyse_text_request
+    user_text = params['user_text']
+    request_params = { user_text: user_text }
+    method = 'analyze_audio'
+    api_request(request_params, method)
+  end
 
+  private
+
+  def api_request(request_params, method)
+    uri = URI("https://api.hear4.me/#{method}")
+    uri.query = URI.encode_www_form(request_params)
     res = Net::HTTP.get_response(uri)
-    puts res.body if res.is_a?(Net::HTTPSuccess)
+    @response = res.body if res.is_a?(Net::HTTPSuccess)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_09_192840) do
+ActiveRecord::Schema.define(version: 2022_04_20_004537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "records", force: :cascade do |t|
+    t.string "filename"
+    t.string "analysis"
+    t.string "transcription"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_04_09_192840) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "records", "users"
 end
